bb7c04990fc7355048b61c82447d62781d206b9d
stonecnz <stonecnz@gmail.com>
2021-06-24 08:07:16 +1200

    created python scipt to count emails from each address

---
+++ pythonExercises/histogram.py
@@ -0,0 +1,28 @@
+# Request the name of the file
+fname = input('Enter the file name: ')
+
+# Try opening the file, but reject and exit if the user's input doesn't match a file
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+
+# Init the emails dict
+emails = dict()
+addy = []
+
+# Find the interesting lines, split into words, add the email address to a list
+for line in fhand:
+    if line.startswith('From'):
+        words = line.split()
+        
+        if len(words) < 2: continue
+
+        addy.append(words[1])
+
+        for add in addy:
+            emails[add] = emails.get(add, 0) + 1
+
+for email in emails:
+    print(email, ':', emails[email])
