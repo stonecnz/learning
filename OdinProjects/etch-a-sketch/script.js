e47dab692eab055cb1a37117e84a284262d0aeb6
stonecnz <stonecnz@gmail.com>
2021-06-30 21:27:36 +1200

    added some functionality but it needs refining

--- OdinProjects/etch-a-sketch/script.js
+++ OdinProjects/etch-a-sketch/script.js
@@ -1,26 +1,37 @@
 const body = document.querySelector('body'); // init the page element
+const submitButton = document.querySelector('.submit');
 
-const gridContainer = document.createElement('div'); // create a div to hold the grid
-gridContainer.classList.add('grid-container'); // give the div a class so that I can add css
+const changeDimensions = () => {
+    let sizeOfGrid = document.querySelector("#size").value;
+    let widthOfCanvas = document.querySelector("#width").value;
+    console.log(sizeOfGrid);
+    if (sizeOfGrid === '' || widthOfCanvas === '') {
+        sizeOfGrid = 90;
+        widthOfCanvas = 900;
+    }
+    createGrid(sizeOfGrid, widthOfCanvas);
+}
 
-body.appendChild(gridContainer); // add the grid-container to the html page
+const createGrid = (size, width) => {
+    let gridContainer = document.createElement('div'); // create a div to hold the grid
+    gridContainer.classList.add('grid-container'); // give the div a class so that I can add css
+    body.appendChild(gridContainer); // add the grid-container to the html page
+    gridContainer.style.width = `${width}px`;
+    gridContainer.style.height = `${width}px`;
 
-const createGrid = (size) => {
-    for (i = 0; i < size; i++) { //create row
+    for (i = 0; i < (size*size); i++) { //create row
         let grid = document.createElement('div');
-        grid.classList.add(`row${i}`);
         grid.classList.add('grid');
+
+        let gridWidth = width/size;
+        grid.style.width = `${gridWidth}px`;
+        grid.style.height = `${gridWidth}px`;
+
         gridContainer.appendChild(grid);
-
-        for (j = 0; j < size; j++) {
-            let grid = document.createElement('div');
-            grid.classList.add('column');
-            grid.classList.add('grid');
-
-            let gridRow = document.querySelector(`.row${i}`);
-            gridRow.appendChild(grid);
-        }
     }
 }
 
-createGrid(16);
+
+submitButton.addEventListener('click', changeDimensions, {
+    once: true
+});
