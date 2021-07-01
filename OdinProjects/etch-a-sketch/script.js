7123d7f77e8babc250a97c4088979d559ad8d6a4
stonecnz <stonecnz@gmail.com>
2021-07-01 21:46:45 +1200

    adding some functionality and cleaned up the UI

--- OdinProjects/etch-a-sketch/script.js
+++ OdinProjects/etch-a-sketch/script.js
@@ -1,37 +1,93 @@
-const body = document.querySelector('body'); // init the page element
 const submitButton = document.querySelector('.submit');
+const invertButton = document.querySelector('.invert');
+const menu = document.querySelector('.menu');
+let pixelSize = 32;
+let defaultColour = 'Purple';
 
-const changeDimensions = () => {
-    let sizeOfGrid = document.querySelector("#size").value;
-    let widthOfCanvas = document.querySelector("#width").value;
-    console.log(sizeOfGrid);
-    if (sizeOfGrid === '' || widthOfCanvas === '') {
-        sizeOfGrid = 90;
-        widthOfCanvas = 900;
-    }
-    createGrid(sizeOfGrid, widthOfCanvas);
-}
+const gridContainer = document.createElement('div'); // create a div to hold the grid
+menu.insertAdjacentElement('afterend', gridContainer); // add the grid-container to the html page
+gridContainer.classList.add('grid-container'); // give the div a class so that I can add css
+let canvasWidth = gridContainer.getBoundingClientRect().width;
 
-const createGrid = (size, width) => {
-    let gridContainer = document.createElement('div'); // create a div to hold the grid
-    gridContainer.classList.add('grid-container'); // give the div a class so that I can add css
-    body.appendChild(gridContainer); // add the grid-container to the html page
-    gridContainer.style.width = `${width}px`;
-    gridContainer.style.height = `${width}px`;
-
-    for (i = 0; i < (size*size); i++) { //create row
-        let grid = document.createElement('div');
+const createGrid = () => {
+    for (i = 0; i < (pixelSize*pixelSize); i++) { //create row
+        const grid = document.createElement('div');
+        gridContainer.appendChild(grid);
         grid.classList.add('grid');
 
-        let gridWidth = width/size;
+        const gridWidth = canvasWidth/pixelSize;
         grid.style.width = `${gridWidth}px`;
         grid.style.height = `${gridWidth}px`;
-
-        gridContainer.appendChild(grid);
     }
+};
+
+createGrid();
+
+let grids = document.querySelectorAll('.grid');
+
+const addDrawAnimation = () => {
+    let grids = document.querySelectorAll('.grid');
+    grids.forEach((grid) => {
+        grid.addEventListener('mouseover', function() {
+            grid.classList.toggle('active');
+        });
+    });
+};
+
+addDrawAnimation();
+
+const clearGrid = () => {
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    };
 }
 
-
-submitButton.addEventListener('click', changeDimensions, {
-    once: true
+submitButton.addEventListener('click', function() {
+    let grids = document.querySelectorAll('.grid');
+    grids.forEach((grid) => {
+        grid.classList.remove('active');
+    });
 });
+
+invertButton.addEventListener('click', function() {
+    let grids = document.querySelectorAll('.grid');
+    grids.forEach((grid) => {
+        grid.classList.toggle('active');
+    });
+});
+
+const sizes = document.querySelectorAll('.size');
+sizes.forEach((size) => {
+    if (Number(size.innerHTML) === pixelSize) {
+        size.classList.add('active-size');
+    }
+    size.addEventListener('click', function() {
+        sizes.forEach((size) => {
+            size.classList.remove('active-size');
+        });
+        size.classList.add('active-size');
+        pixelSize = Number(size.innerHTML);
+        clearGrid();
+        createGrid();
+        addDrawAnimation();
+    });
+});
+
+const colours = document.querySelectorAll('.colour');
+colours.forEach((colour) => {
+    if (colour.innerHTML === defaultColour) {
+        colour.classList.add('active-size');
+    };
+    colour.addEventListener('click', function() {
+        colours.forEach((colour) => {
+            colour.classList.remove('active-size');
+        });
+        colour.classList.add('active-size');
+        defaultColour = colour.innerHTML;
+        let sheet = document.styleSheets[0];
+        let rules = sheet.cssRules || sheet.rules;
+        rules[9].style.backgroundColor = defaultColour;
+        console.log(rules);
+    });
+});
+
