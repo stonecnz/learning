b144da3f92e58662b79e05ec9db7b8f96c7c63e5
stonecnz <stonecnz@gmail.com>
2021-07-21 20:40:05 +1200

    just trying to build the form

--- OdinProjects/library-app/script.js
+++ OdinProjects/library-app/script.js
@@ -1,5 +1,13 @@
 let myLibrary = [];
+
 const bookContainer = document.querySelector('.bookContainer');
+const addBookButton = document.querySelector('.add-book-button'); 
+const formPopup = document.querySelector('.form-popup');
+const newBookSubmitButton = document.querySelector('.submit-btn');
+const titleInput = document.querySelector('.title-input');
+const authorInput = document.querySelector('.author-input');
+const pagesInput = document.querySelector('.pages-input');
+const hasReadInput = document.querySelector('.hasRead-input');
 
 function Book(title, author, pages, hasRead) {
     this.title = title
@@ -39,4 +47,30 @@ myLibrary.forEach(book => {
         bookProp.classList.add('bookProp');
     }) 
 
+})
+
+addBookButton.addEventListener('click', function() {
+    if (formPopup.style.display == "none") {
+        formPopup.style.display = "block";
+    } else {
+        formPopup.style.display = "none";
+    }
+    
+})
+
+newBookSubmitButton.addEventListener('click', function() {
+    console.log(titleInput.value, authorInput.value, pagesInput.value, hasReadInput.value);
+    let x = new Book(titleInput.value, authorInput.value, pagesInput.value, hasReadInput.value);
+    addBookToLibrary(x);
+    console.log(myLibrary);
+    let bookEntry = document.createElement('div');
+    bookContainer.appendChild(bookEntry);
+    bookEntry.classList.add('bookEntry');
+    let lastBook = myLibrary.length;
+    Object.keys(myLibrary[lastBook - 1]).forEach(key => {
+        let bookProp = document.createElement('p');
+        bookProp.innerHTML = myLibrary[lastBook - 1][key];
+        bookEntry.appendChild(bookProp);
+        bookProp.classList.add('bookProp');
+    }) 
 })
