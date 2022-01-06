7c3099ffd0de79d8d51ff860be795ff59c330438
stonesolidimpact <caleb@stonesolidimpact.com>
2022-01-07 10:46:30 +1300

    Added the checkwinner function. But, I need to improve it and make it less gross ><

--- OdinProjects/tic-tac-toe/script.js
+++ OdinProjects/tic-tac-toe/script.js
@@ -1,9 +1,29 @@
 // the gameboard has 9 values that relate to the 9 spaces on the board. These values can either be 'x' or 'o'.
-// The gameboard contains a function to render the 9 game squares on the screen. Each of these gamesquare should come with an event listener.
-// 
+// The gameboard contains a function to render the 9 game squares on the screen. 
+
+// input: an array of 9; output: a 3x3 grid with each square containing data from an element in the array 
+// create an array that has nine data points, but contains no data
+// create a 3x3 grid with the data from the array
+
+// input: player clicks on a square in the grid; output: a marker is placed within that square
+// listen for a click on a square
+// identify whether it was the player or the AI's turn
+// store the appropriate marker and index
+// change the data in the array at the appropriate index
+// render the change to the 3x3 grid (if AI, render this after a slight delay)
+// check whether a win condition has been satisfied - its own problem
+
+// win-condition
+// input: an array of 9 data points; output: a boolean determining whether a win condition has been satisfied and the winners name
+// look through elements
+// check if one of the 8 win-conditions has been satisfied
+// determine which player satisfied the win-condition
+// return a boolean which identifies current win-condition and if true then who satisfied it
+
+
 
 const gameboard = (() => { // using a module pattern to create the gameBoard object
-    let gameboardArr = ['', '', '', '', '', '', '', '', '']; // initialising the array to hold each of the squares on the gameboard
+    let gameboardArr = Array.from({length: 9},(i) => ''); // initialising the array to hold each of the squares on the gameboard
     
     const renderGameboard = () => {
         let board = document.querySelector('.game-board');
@@ -16,10 +36,12 @@ const gameboard = (() => { // using a module pattern to create the gameBoard obj
             square.innerText = x;
             square.classList = 'game-square';
             square.setAttribute('data-index', index);
+            
             square.addEventListener('click', function() {
                 if (!this.innerText) {
-                    gameboardArr[this.dataset.index] = 'x';
+                    gameboardArr[this.dataset.index] = 'o';
                     renderGameboard();
+                    checkWinCondition();
                 } else {
                     console.log('No!');
                 };
@@ -28,6 +50,49 @@ const gameboard = (() => { // using a module pattern to create the gameBoard obj
             index ++;
         });
     }
+
+    const checkWinCondition = () => {
+        let winCondition = false;
+        let winner = null;
+        // identify the 8 win conditions
+        const winConditions = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]]; 
+
+        // look through elements and check if one of the 8 win-conditions has been satisfied
+        winConditions.forEach(condition => {
+            let xCount = 0;
+            let oCount = 0;
+                condition.forEach(index => {
+                    if (gameboardArr[index] == 'x') {
+                        xCount ++;    
+                    } else if (gameboardArr[index] == 'o') {
+                        oCount ++;
+                    }
+                })
+            if (xCount == 3) {
+                winCondition = true;
+                winner = 'x';
+            } else if (oCount == 3) {
+                winCondition = true;
+                winner = 'o';
+            }
+        }) 
+
+        // return a boolean which identifies current win-condition and if true then who satisfied it
+        console.log(winCondition, winner);
+        return {
+            winCondition, 
+            winner
+        }
+    }
+    
 
     const setMarker = (index, marker) => {
         gameboardArr[index-1] = marker;
@@ -46,6 +111,7 @@ const displayController = (() => { // creating the object to control the flow of
 
 const Player = (name) => { // creating a factor for the objects that play the game
     let numWins = 0;
+    let marker = 'x'
     const getName = () => name;
 
 }
