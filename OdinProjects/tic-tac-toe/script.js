45ddabfeca73290be5f6a1e4a32f0895ac6b04eb
stonesolidimpact <caleb@stonesolidimpact.com>
2022-01-08 12:07:31 +1300

    basic functionality working

--- OdinProjects/tic-tac-toe/script.js
+++ OdinProjects/tic-tac-toe/script.js
@@ -1,59 +1,92 @@
-// the gameboard has 9 values that relate to the 9 spaces on the board. These values can either be 'x' or 'o'.
-// The gameboard contains a function to render the 9 game squares on the screen. 
+const gameSquares = document.querySelectorAll('.game-square');
+const message = document.querySelector('.message');
 
-// input: an array of 9; output: a 3x3 grid with each square containing data from an element in the array 
-// create an array that has nine data points, but contains no data
-// create a 3x3 grid with the data from the array
+// using a module pattern to create the gameBoard object
+const gameboard = (() => { 
+    // creating an array to hold each of the squares on the gameboard
+    let gameboardArr = ['','','','','','','','','']; 
 
-// input: player clicks on a square in the grid; output: a marker is placed within that square
-// listen for a click on a square
-// identify whether it was the player or the AI's turn
-// store the appropriate marker and index
-// change the data in the array at the appropriate index
-// render the change to the 3x3 grid (if AI, render this after a slight delay)
-// check whether a win condition has been satisfied - its own problem
-
-// win-condition
-// input: an array of 9 data points; output: a boolean determining whether a win condition has been satisfied and the winners name
-// look through elements
-// check if one of the 8 win-conditions has been satisfied
-// determine which player satisfied the win-condition
-// return a boolean which identifies current win-condition and if true then who satisfied it
-
-
-
-const gameboard = (() => { // using a module pattern to create the gameBoard object
-    let gameboardArr = Array.from({length: 9},(i) => ''); // initialising the array to hold each of the squares on the gameboard
-    
-    const renderGameboard = () => {
-        let board = document.querySelector('.game-board');
-        while(board.lastChild) {
-            board.removeChild(board.lastChild);
+    const getGameSquare = (index) => {
+        if (index < gameboardArr.length) {
+            return gameboardArr[index];
         }
-        let index = 0;
-        gameboardArr.forEach(x => {
-            let square = document.createElement('div');
-            square.innerText = x;
-            square.classList = 'game-square';
-            square.setAttribute('data-index', index);
-            
-            square.addEventListener('click', function() {
-                if (!this.innerText) {
-                    gameboardArr[this.dataset.index] = 'o';
-                    renderGameboard();
-                    checkWinCondition();
-                } else {
-                    console.log('No!');
-                };
-            })
-            board.appendChild(square);
-            index ++;
-        });
     }
 
-    const checkWinCondition = () => {
-        let winCondition = false;
-        let winner = null;
+    // the current move in a round: even = x; odd = o.
+    let currentMove = 0;
+
+    const getMarker = () => {
+        if (currentMove % 2 === 0) {
+            return 'X';
+        } else {
+            return 'O';
+        }
+    };
+
+    const updateTurnMessage = () => {
+        message.textContent = `Player ${getMarker()}'s turn`;
+    }
+
+    updateTurnMessage();
+
+
+    // status of game
+    let gameStatus = true;
+
+    // add an event listener to the squares so that we can place markers then update the board
+    gameSquares.forEach((gameSquare) => {
+        gameSquare.addEventListener('click', (e) => {
+            if (gameStatus && e.target.textContent === '') {    
+                placeMarker(e.target.dataset.index);
+                updateGameboard();
+                if (displayController.checkWinCondition(parseInt(e.target.dataset.index), e.target.textContent)) {
+                    gameStatus = false;
+                    displayController.displayWinner();
+                } else {
+                    currentMove ++;
+                    updateTurnMessage();
+                };
+            }    
+        })
+    });
+
+    // depending on who is currently playing, adds the player's marker into the gameboardArray
+    const placeMarker = (index) => {
+        if (index < gameboardArr.length) {
+            gameboardArr[index] = getMarker();
+        }
+    };
+
+    //Update the gameboard by iterating through the node list and matching the array to each in the list
+    const updateGameboard = () => {
+        for (let i = 0; i < gameSquares.length; i++) {
+            gameSquares[i].textContent = gameboardArr[i];            
+        }
+    }
+    updateGameboard();
+
+    // function to be used to reset when the reset button is clicked.
+    const resetGameboard = () => {
+        gameboardArr.fill('');
+        updateGameboard();
+        currentMove = 0;
+        updateTurnMessage();
+    }
+
+    const resetButton = document.querySelector('.reset');
+    resetButton.addEventListener('click', resetGameboard);
+    
+    return {
+        gameboardArr,
+        getGameSquare,
+        getMarker
+    };
+})();
+
+const displayController = (() => { // creating the object to control the flow of the game itself
+
+    // check to see whether anyone has satisfied a winning condition yet
+    const checkWinCondition = (squareIndex, marker) => {
         // identify the 8 win conditions
         const winConditions = [
             [0,1,2],
@@ -65,61 +98,28 @@ const gameboard = (() => { // using a module pattern to create the gameBoard obj
             [0,4,8],
             [2,4,6]]; 
 
-        // look through elements and check if one of the 8 win-conditions has been satisfied
-        winConditions.forEach(condition => {
-            let xCount = 0;
-            let oCount = 0;
-                condition.forEach(index => {
-                    if (gameboardArr[index] == 'x') {
-                        xCount ++;    
-                    } else if (gameboardArr[index] == 'o') {
-                        oCount ++;
-                    }
-                })
-            if (xCount == 3) {
-                winCondition = true;
-                winner = 'x';
-            } else if (oCount == 3) {
-                winCondition = true;
-                winner = 'o';
-            }
-        }) 
-
-        // return a boolean which identifies current win-condition and if true then who satisfied it
-        console.log(winCondition, winner);
-        return {
-            winCondition, 
-            winner
-        }
+        // for each array in the array, filter checks whether they include the index of the square that was just played. Filter then returns an array that contains only those win conditions that contain the played index. Some then checks whether every square at the indices contained in the arrays match the player's marker. If they do, then it returns true.
+        return winConditions
+            .filter((condition) => condition.includes(squareIndex))
+            .some((possibleCondition) => possibleCondition.every(
+                (index) => gameboard.getGameSquare(index) === marker));
     }
-    
 
-    const setMarker = (index, marker) => {
-        gameboardArr[index-1] = marker;
-        renderGameboard();
+    const displayWinner = () => {
+        message.textContent = `Player ${gameboard.getMarker()} won!`
     }
-    
+
     return {
-        renderGameboard,
-        setMarker
+        checkWinCondition,
+        displayWinner
     };
-})();
-
-const displayController = (() => { // creating the object to control the flow of the game itself
 
 })();
 
 const Player = (name) => { // creating a factor for the objects that play the game
-    let numWins = 0;
-    let marker = 'x'
-    const getName = () => name;
 
 }
 
 const AI = () => { // creating a factor for the AI player that inherets methods and properties from the player object
 
 }
-
-
-gameboard.renderGameboard();
-
