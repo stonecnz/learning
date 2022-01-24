bac6d5cd8c8ac3362603e714acdd6522ec945c8e
stonecnz <stonecnz@gmail.com>
2022-01-25 07:22:35 +1300

    removed cookie code that was commented out anyway

--- OdinProjects/library-app/script.js
+++ OdinProjects/library-app/script.js
@@ -1,29 +1,3 @@
-// function setCookie(name,value,exp_days) {
-//     let d = new Date();
-//     d.setTime(d.getTime() + (exp_days*24*60*60*1000));
-//     let expires = "expires=" + d.toGMTString();
-//     document.cookie = name + "=" + value + ";" + expires;
-// }
-
-// function getCookie(name) {
-//     let cname = name + "=";
-//     let decodedCookie = decodeURIComponent(document.cookie);
-//     let ca = decodedCookie.split(';');
-//     for(let i = 0; i < ca.length; i++){
-//         let c = ca[i];
-//         while(c.charAt(0) == ' '){
-//             c = c.substring(1);
-//         }
-//         if(c.indexOf(cname) == 0){
-//             return c.substring(cname.length, c.length);
-//         }
-//     }
-//     return "";
-// }
-// setCookie("myLibrary", "[{title: 'what the dog doing?', author: 'me', pages: 10, read: false}]", 5);
-// let poop = getCookie("myLibrary");
-// console.log(poop);
-
 let myLibrary = [
     {
         title: "What the dog doing?",
