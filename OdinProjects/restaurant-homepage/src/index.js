ed10ea975da5ee788edf2b862c02563046a2612a
stonecnz <stonecnz@gmail.com>
2022-02-01 08:09:30 +1300

    added an icon and a slight background color on the header

--- OdinProjects/restaurant-homepage/src/index.js
+++ OdinProjects/restaurant-homepage/src/index.js
@@ -1,5 +1,11 @@
 import navComponent from "./navComponent.js";
 import "./style.css";
+import Icon from "./book.png";
+
+const myIcon = new Image();
+myIcon.src = Icon;
+myIcon.classList.add("nav-icon");
 
 const container = document.querySelector(".container");
+container.appendChild(myIcon);
 container.appendChild(navComponent());
